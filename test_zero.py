3e648e58bdadb9d77b52e1e0d951a8bb2bca937d
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-02-07 17:33:26 +0100

    Update test_zero.py

--- test_zero.py
+++ test_zero.py
@@ -24,7 +24,7 @@ def test_remote():
     # profile.set_preference('dom.webnotifications.enabled', False)
 
     capabilities = DesiredCapabilities.FIREFOX  # .copy()
-    capabilities['marionette'] = True
+    capabilities['marionette'] = False
     #capabilities['platform'] = "WINDOWS"
     #capabilities['version'] = "7"
 
