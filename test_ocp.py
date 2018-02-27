588a7e142ceefd4c61dde340efc28bee308f2a93
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-02-27 11:36:32 +0100

    Update test_ocp.py

--- test_ocp.py
+++ test_ocp.py
@@ -23,13 +23,12 @@ def test_remote():
     # profile.accept_untrusted_certs = True
     # profile.set_preference('dom.webnotifications.enabled', False)
 
-    
-    #capabilities['marionette'] = False
+    capabilities = DesiredCapabilities.CHROME
+    capabilities['marionette'] = False
     #capabilities['platform'] = "WINDOWS"
     #capabilities['version'] = "7"
 
-    driver = webdriver.Remote(command_executor='http://10.130.3.83:4444/wd/hub',
-                              desired_capabilities=DesiredCapabilities.FIREFOX)
+    driver = webdriver.Remote(command_executor='http://172.30.94.225:4444/wd/hub',desired_capabilities=capabilities)
     driver.get("http://www.python.org")
     assert "Python" in driver.title
 
