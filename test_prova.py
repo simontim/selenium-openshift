e2fe1883858ba0aa3094f77d780aa7cb45e46f79
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-07-09 09:18:00 +0200

    Update test_prova.py

--- test_prova.py
+++ test_prova.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
-def test_sel1():
+def test_remote():
 
     capabilities = DesiredCapabilities.CHROME
     driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub',desired_capabilities=capabilities)
     driver.get("http://www.python.org")
     assert "Python" in driver.title
     driver.quit()
-    
